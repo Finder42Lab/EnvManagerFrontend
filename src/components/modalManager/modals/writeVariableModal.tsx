782bae96b3dbd6829744e71444b362d85263bb29
Dabchansky <d.krasylnikov@arktech.ai>
2025-11-23 19:51:57 +0300

    Правки

--- src/components/modalManager/modals/writeVariableModal.tsx
+++ src/components/modalManager/modals/writeVariableModal.tsx
@@ -64,7 +64,7 @@ const WriteVariableModal: FC<IProps> = ({ variableId, projectId }) => {
 
 		onSuccess: () => {
 			qc.invalidateQueries({ queryKey: ['projectVariables', projectId] })
-			close()
+			form.reset({ name: '', value: '' })
 		},
 	})
 
